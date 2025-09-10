3b49af6c066cb1b72e6c2093efbdc874246b8fd8
Arun V <vatearun@.com>
2025-09-10 19:43:37 +0530

    Created some more REST APIs

--- src/main/java/com/arun/Bank/controller/AccountController.java
+++ src/main/java/com/arun/Bank/controller/AccountController.java
@@ -4,10 +4,10 @@ import com.arun.Bank.dto.AccountDto;
 import com.arun.Bank.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/accounts")
@@ -23,4 +23,42 @@ public class AccountController {
     public ResponseEntity<AccountDto> addAccount(@RequestBody AccountDto accountDto){
         return new ResponseEntity<>(accountService.createAccount(accountDto), HttpStatus.CREATED);
     }
+
+    // Get Account REST API
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id){
+        AccountDto accountDto = accountService.getAccountById(id);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    // Deposit REST API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id,@RequestBody Map<String,Double> request){
+        Double amount=request.get("amount");
+        AccountDto accountDto = accountService.deposit(id,amount);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    // Withdraw REST API
+    @PutMapping("/{id}/withdraw")
+    public ResponseEntity<AccountDto> withdraw(@PathVariable Long id,@RequestBody Map<String,Double> request){
+        Double amount=request.get("amount");
+        AccountDto accountDto = accountService.withdraw(id,amount);
+        return ResponseEntity.ok(accountDto);
+    }
+
+    // Get All Accounts REST API
+    @GetMapping
+    public ResponseEntity<List<AccountDto>> getAllAccounts(){
+        List<AccountDto> accounts = accountService.getAllAccounts();
+        return ResponseEntity.ok(accounts);
+    }
+
+    // Delete Account REST API
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteAccounts(@PathVariable Long id){
+        accountService.deleteAccount(id);
+        return ResponseEntity.ok("Account is Deleted Successfully");
+    }
+
 }
--- src/main/java/com/arun/Bank/service/AccountService.java
+++ src/main/java/com/arun/Bank/service/AccountService.java
@@ -2,6 +2,14 @@ package com.arun.Bank.service;
 
 import com.arun.Bank.dto.AccountDto;
 
+import java.util.List;
+
 public interface AccountService {
     AccountDto createAccount(AccountDto accountDto);
+    AccountDto getAccountById(Long id);
+    AccountDto deposit(Long id,double amount);
+    AccountDto withdraw(Long id,double amount);
+    List<AccountDto> getAllAccounts();
+    void deleteAccount(Long id);
+
 }
